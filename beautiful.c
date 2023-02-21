0fb266aaa8951ac1778188251bbb5fb0de60460b
timmoh-king <timothykariuki43@gmail.com>
2023-02-21 03:57:30 -0800

    Task 0

---
+++ beautiful.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Welcome to the simple shell project\n");
+
+	return (0);
+}
