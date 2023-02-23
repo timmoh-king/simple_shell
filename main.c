194e023475675a5782f9058cb8f50caa5d01b97b
erastusiam <erastusmakena@gmail.com>
2023-02-23 11:59:32 -0800

    Updated file

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
  * main - entry point
  * @ac: arg count
  * @av: arg vector
@@ -42,67 +41,4 @@ int main(int ac, char **av)
 	read_history(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
-=======
- * main - the main function
- * @ac: number of arguments passed
- * @argv: arguments passed
- *
- * Return: Always 0 (Success)
- */
-
-int main(int ac, char **argv)
-{
-	char *prompt = "(cisfun) $ ";
-	size_t n = 0;
-	ssize_t nchars_read;
-	char **av, *lineptr = NULL;
-
-	/*declaring void variables*/
-	(void)ac;
-	error = argv;
-
-	/*create an infinite loop*/
-	while (1)
-	{
-		printf("%s", prompt);
-		nchars_read = getline(&lineptr, &n, stdin);
-
-		/*check if the getline function failed or reached EOF*/
-		if (nchars_read == -1)
-		{
-			printf("\n");
-			free(lineptr);
-			return (0);
-		}
-		av = get_arguments(lineptr);
-		execmd(av);
-	}
-
-	return (0);
-}
-/**
- * get_arguments - get arguments passed on terminal
- * @lineptr: pointer to the line args
- *
- * Return: array of strings
- */
-char **get_arguments(char *lineptr)
-{
-	const char *delim = " \r\t\n";
-	char *token;
-	int i = 0;
-	char **argv = malloc(sizeof(char *) * 100);
-
-	token = strtok(lineptr, delim);
-	while (token)
-	{
-		argv[i] = token;
-		i++;
-
-		token = strtok(NULL, delim);
-	}
-	argv[i] = NULL;
-
-	return (argv);
->>>>>>> 9c7aebbcb8c531cfd94f22078050f30786820a6e
-}
+}	{
